fe2b8c3743e43ccfceab38a60ace29ed885349a9
robert0203 <reifschneider0203@gmail.com>
2022-05-03 18:32:39 +0200

    setzt NUmmern auf 0 bzw. Anfang; querySelector eingebaut für verschiedenen Tips

--- src/inputNumbers.ts
+++ src/inputNumbers.ts
@@ -3,7 +3,14 @@ const numberPeople: HTMLInputElement = <HTMLInputElement>document.getElementById
 const numberCustom: HTMLInputElement = <HTMLInputElement>document.getElementById("customButton")
 const numberTotal: HTMLElement = <HTMLElement>document.getElementById("amountTotal")
 const numberTip: HTMLElement = <HTMLElement>document.getElementById("amountTip")
+const numberPercent: NodeList = <NodeList>document.querySelectorAll(".button")
 
 
 
-export {numberBill, numberPeople, numberCustom, numberTip, numberTotal}
+numberBill.value = "0.00";
+numberPeople.value = "1";
+numberTip.innerHTML = "€ " + (0.00).toFixed(2)
+numberTotal.innerHTML = "€ " + (0.00).toFixed(2)
+
+
+export {numberBill, numberPeople, numberCustom, numberTip, numberTotal, numberPercent}
