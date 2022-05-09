7c0f3f188a26a1119cd96fb343f12a055490c0b9
robert0203 <reifschneider0203@gmail.com>
2022-05-09 09:46:27 +0200

    Kommentare hinzugefügt und Überflüssiges gelöscht

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import { calculateTip } from "./calculateTip";
 import { amountBill, amountPeople, billInput, peopleInput } from "./eventListener";
 import { numberPercent } from "./inputNumbers";
 import { percent } from "./percent";
-
 peopleInput();
 billInput();
 
-numberPercent.forEach(button => console.log(button.textContent));
 
-console.log(percent("5%"));
